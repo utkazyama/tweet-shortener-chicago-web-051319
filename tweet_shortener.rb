dc6bca6c68eb0bc9084a90fa696589f161d20075
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-26 03:36:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,7 +39,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length>140
-      word_substituter(tweet)[0..137] +"..."
+      word_substituter(tweet)[0..136] +"..."
     else
       word_substituter(tweet)
      end
