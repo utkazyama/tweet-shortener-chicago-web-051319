fbc08e50d79ea7ca47f2b7edc084603feab9b2c9
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-26 03:27:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,45 @@
-# Write your code here.
+def dictionary
+  list={
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet_one)
+  tweet_one.split.collect do |word|
+   if dictionary.keys.include? (word.downcase)
+     word=dictionary[word.downcase]
+   else
+     word
+   end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do
+    puts word_substituter(tweets)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length >140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+end
+  
+
+  
+  
